638f83e7a98844ad0aaf07ca0951da3dfa4bede7
Domantas Puišys <digitalsquidnetwork@gmail.com>
2018-04-09 17:17:21 +0300

    Global duomenu bazes info ir prisijungimas

--- MyWebsite/pages/server.php
+++ MyWebsite/pages/server.php
@@ -13,15 +13,15 @@ $email = "";
 $errors = array();
 
 // connect to the database
-$db = mysqli_connect('localhost', 'root', '', 'dashboard');
+include 'mysql.php';
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
 	// receive all input values from the form
-	$username = mysqli_real_escape_string($db, $_POST['username']);
-	$email = mysqli_real_escape_string($db, $_POST['email']);
-	$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-	$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+	$username = mysqli_real_escape_string($connect, $_POST['username']);
+	$email = mysqli_real_escape_string($connect, $_POST['email']);
+	$password_1 = mysqli_real_escape_string($connect, $_POST['password_1']);
+	$password_2 = mysqli_real_escape_string($connect, $_POST['password_2']);
 
 	// form validation: ensure that the form is correctly filled ...
 	// by adding (array_push()) corresponding error unto $errors array
@@ -35,7 +35,7 @@ if (isset($_POST['reg_user'])) {
 	// first check the database to make sure
 	// a user does not already exist with the same username and/or email
 	$user_check_query = "SELECT * FROM vartotojai WHERE username='$username' OR email='$email' LIMIT 1";
-	$result = mysqli_query($db, $user_check_query);
+	$result = mysqli_query($connect, $user_check_query);
 	$user = mysqli_fetch_assoc($result);
 
 	if ($user) {
@@ -55,7 +55,7 @@ if (isset($_POST['reg_user'])) {
 
 		$query = "INSERT INTO vartotojai (username, email, password, type)
   			  VALUES('$username', '$email', '$password', 1)";
-		mysqli_query($db, $query);
+		mysqli_query($connect, $query);
 		$_SESSION['username'] = $username;
 		// sukuria folderi su $username folderyje /users
 		mkdir('../users/' . $username, true);
@@ -65,8 +65,8 @@ if (isset($_POST['reg_user'])) {
 }
 
 if (isset($_POST['login_user'])) {
-	$username = mysqli_real_escape_string($db, $_POST['username']);
-	$password = mysqli_real_escape_string($db, $_POST['password']);
+	$username = mysqli_real_escape_string($connect, $_POST['username']);
+	$password = mysqli_real_escape_string($connect, $_POST['password']);
 
 	if (empty($username)) {
 		array_push($errors, "Username is required");
@@ -78,7 +78,7 @@ if (isset($_POST['login_user'])) {
 	if (count($errors) == 0) {
 		$password = md5($password);
 		$query = "SELECT * FROM vartotojai WHERE username='$username' AND password='$password'";
-		$results = mysqli_query($db, $query);
+		$results = mysqli_query($connect, $query);
 		if (mysqli_num_rows($results) == 1) {
 			$_SESSION['username'] = $username;
 			$_SESSION['success'] = "You are now logged in";
